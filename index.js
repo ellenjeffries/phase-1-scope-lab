e5a146a0019130e1528dfa0803e8c510bde8da9d
ellen <ellenjojeffries@gmail.com>
2024-06-12 11:44:34 -0600

    index.js

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your solution in this file!
+var customerName = 'bob'
+
+function upperCaseCustomerName() {
+    customerName = customerName.toUpperCase()
+}
+function setBestCustomer() {
+    bestCustomer = 'not bob'
+    return bestCustomer
+}
+function overwriteBestCustomer() {
+     bestCustomer = 'maybe bob'
+}
+const leastFavoriteCustomer = 'molly'
+
+function changeLeastFavoriteCustomer() {
+    return leastFavoriteCustomer = 'sarah'
+}
